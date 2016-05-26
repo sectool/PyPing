ed0c7dbaafea9576221a410c7a58c59f6bab8bce
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-05-26 14:50:58 -0400

    Add files via upload

---
+++ ping_test_tool.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import time
+import socket
+
+def ping_test():
+	ip_adres = str(raw_input("Ping testi için bir ip adresi giriniz : "))
+	ping_adet = str(raw_input("Ping testi için kaç tane ping göndermek istiyorsunuz : "))
+	os.system("ping" + " " + "-c" + ping_adet + " " + ip_adres)
+
+ping_test_ico = """
+#########################################################
+#       PYTHON - PING TEST T00L - GH0ST S0FTWARE        #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#        Mail Address : pentestdatabase@gmail.com       #
+# LINKEDIN : https://www.linkedin.com/in/ismailtasdelen #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+"""
+	
+print ping_test_ico	
+
+
+star = "#########################################################";
+
+print star
+
+ping_test()
+
+print star
